3a31e5c8185ee66071e9c89d0dedc8dbb47eced9
Paul Kruger and Peter Azcue <pazcue+miamiruby+pazcue@hotmail.com>
2015-03-27 16:22:09 -0400

    adding movies class

--- lib/class_movies.rb
+++ lib/class_movies.rb
@@ -1,14 +1,10 @@
 class Movies 
-
-	attr_reader :array
-	def search
-		i = Imdb::Search.new("Funny")
-
-		puts i.movies.size
-
-		#.title 
-		#=> 97
-		return i.movies
+	def search(word)
+		searched_movies = Imdb::Search.new(word).movies
+        random_movies = searched_movies.shuffle.first(15)
+        movies = random_movies.map{|m|
+           {title: m.title, poster: m.poster}
+        }.delete_if{|m| m[:poster] == nil}
+        movies.first(9)
 	end
-	 
 end
